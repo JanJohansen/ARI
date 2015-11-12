a47c7fa9ab3e194141d31da2aade8d61296b1258
Jesper Berthing <jberthing@gmail.com>
2015-11-12 19:43:45 +0100

    activating and deactivating nodes in MysensorsGW plugin

--- ARI/plugins/MysensorsGW/www/settings.js
+++ ARI/plugins/MysensorsGW/www/settings.js
@@ -4,12 +4,12 @@ var ariModule = angular.module('ari');
 ariModule.register.controller('MysensorsGWSettingsController', ['$scope', 'AriClient', "AriUser",
     function ($scope, AriClient, AriUser) {
         $scope.clientInfo = {};
-        
+
         var ari = AriClient.create("MysensorsGW_Settings");
-        
+
         ari.onconnect = function (result) {
             var clientName = result.name;
-            
+
             // Get list of clients.
             ari.callRpc("MysensorsGW.getConfig", {}, function (err, result) {
                 if (err) { console.log(err); return; }
@@ -18,15 +18,37 @@ ariModule.register.controller('MysensorsGWSettingsController', ['$scope', 'AriCl
                 $scope.config = result;
                 $scope.$apply();    // make sure nGular treats the update!
             });
-            
+
             $scope.saveConfig = function ()
             {
                 ari.callRpc("MysensorsGW.setConfig", $scope.config, function (err, result) {
                     if (err) { console.log(err); return; }
                 });
             }
+
+            $scope.activateNodeId = function (nodeId)
+            {
+              var node = $scope.config.NotAdded[nodeId];
+              $scope.config.nodes[nodeId] = node;
+              $scope.config.NotAdded[nodeId] = null;
+              delete $scope.config.NotAdded[nodeId];
+              $scope.$apply();
+            }
+
+            $scope.deActivateNodeId = function (nodeId)
+            {
+              var node = $scope.config.nodes[nodeId];
+              $scope.config.NotAdded[nodeId] = node;
+              $scope.config.nodes[nodeId] = null;
+              delete $scope.config.nodes[nodeId];
+              $scope.$apply();
+            }
+
+
         };
-        
+
+
+
         $scope.$on('$destroy', function () {
             //if ($scope.clientInfo.name) ari.unsubscribe($scope.clientInfo.name + ".*");
             if (ari) ari.close();
@@ -34,4 +56,3 @@ ariModule.register.controller('MysensorsGWSettingsController', ['$scope', 'AriCl
         });
     }
 ]);
-
