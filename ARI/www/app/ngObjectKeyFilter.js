f54176c3c50a8ef4b6cd2ddd3c4abd6a21d23fc7
Jan Johansen <Jan@JohansenWeb.dk>
2016-02-09 17:56:00 +0100

    Fixed issue with angular filter not working on Object Keys after upgrade... New objectKeyFilter created.

---
+++ ARI/www/app/ngObjectKeyFilter.js
@@ -0,0 +1,16 @@
+﻿angular.module('ari')
+.filter('objectKeyFilter', function () {
+    return function (input, search) {
+        if (!input) return input;
+        if (!search) return input;
+        var expected = ('' + search).toLowerCase();
+        var result = {};
+        angular.forEach(input, function (value, key) {
+            var actual = ('' + key).toLowerCase();
+            if (actual.indexOf(expected) !== -1) {
+                result[key] = value;
+            }
+        });
+        return result;
+    }
+});
